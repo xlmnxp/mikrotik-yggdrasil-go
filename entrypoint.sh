473f53f23086ea605b521893e704f6a1c2199fe5
Salem Yaslem <s@sy.sa>
2023-09-25 23:43:34 +0300

    add ip forwarding

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -m
+
+echo "Hello Khalid"
+
+echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.ccoonf
+sysctl -p /etc/sysctl.conf
+
 #set -euxo pipefail
 
 conf_file=/config/yggdrasil.conf
@@ -7,7 +15,7 @@ conf_file=/config/yggdrasil.conf
 [ -f "${conf_file}" ] || yggdrasil -genconf >${conf_file}
 
 sed -i 's/IfName: auto/IfName: none/g' ${conf_file}
-sed -i 's/AdminListen: unix:\/\/\/var\/run\/yggdrasil.sock/AdminListen: none/g' ${conf_file}
+sed -i 's/AdminListen: unix:\/\/\/var\/run\/yggdrasil\/yggdrasil.sock/AdminListen: none/g' ${conf_file}
 sed -i 's/Listen: \[\]/Listen: \[ "tcp:\/\/0.0.0.0:3434" \]/g' ${conf_file}
 
 exec /usr/bin/yggdrasil -useconffile ${conf_file}
