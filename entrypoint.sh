76de3f4af3dbf17fb8c46b64865cf04141088506
Salem Yaslem <s@sy.sa>
2023-09-25 23:51:09 +0300

    add ip forwarding

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -m
 echo "Hello Khalid"
 
 echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.ccoonf
+echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
 
 #set -euxo pipefail
